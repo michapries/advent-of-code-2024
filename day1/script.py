12304b96e164aeb086c4ef0984a08d90573f6dd1
Micha-Alexander Pries <micha-alexander.pries@uni-bielefeld.de>
2024-12-01 15:53:56 +0100

    Renaming

--- day1/script.py
+++ day1/script.py
@@ -2,13 +2,15 @@ import numpy as np
 import pandas as pd
 import datetime
 import math
+from collections import Counter
+
 
 def get_day():
     return datetime.datetime.today().day
 
 
 def get_input():
-    with open(f'./day{get_day()}/input.txt', 'r') as input:
+    with open(f'./day1/input.txt', 'r') as input:
         return input.readlines()
     
 
@@ -16,23 +18,24 @@ def get_testinput(task):
     if task not in [1, 2]:
         raise Exception('Not a valid task.')
     else:
-        with open(f'./day{get_day()}/testinput{task}.txt', 'r') as input:
+        with open(f'./day1/testinput{task}.txt', 'r') as input:
             return input.readlines()
 
 
-def part1():
-    return
-
-
-def part2():
-    return
-
-
 def main(is_part1):
+    #input = [x.strip('\n').split() for x in get_testinput(1)]
+    input = [x.strip('\n').split() for x in get_input()]
+
+    # "Transpose" lists.
+    left, right = [int(item[0]) for item in input], [int(item[1]) for item in input]
+
     if is_part1:
-        return part1()
+        # Elementwise subtraction between sorted left and right lists.
+        diffs = [abs(l - r) for l, r in zip(sorted(left), sorted(right))]
+        return sum(diffs)
     else:
-        return part2()
+        counter_right = Counter(right)
+        return sum([l  * counter_right[l] for l in left])
 
 
 print("Part 1:", main(True))
