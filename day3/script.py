dab26fc72adc8bf200418efd6ca15a0e3d0fe0c2
Micha-Alexander Pries <micha-alexander.pries@uni-bielefeld.de>
2024-12-03 19:06:43 +0100

    Rename variable

--- day3/script.py
+++ day3/script.py
@@ -7,17 +7,17 @@ def get_input(testinput=0):
             
 
 def main(is_part1):
-    input = ''.join(get_input())
+    text = ''.join(get_input())
 
     if not(is_part1):
         # Split at every do().
-        do_split = re.split("do\(\)", input)
+        do_split = re.split("do\(\)", text)
 
         # For every entry beginning with do, cut off everything after "don't".
-        input = ''.join([re.split("don't\(\)", x)[0] for x in do_split])
+        text = ''.join([re.split("don't\(\)", x)[0] for x in do_split])
 
     # List of matching mul strings.
-    muls = re.findall('mul\(\d{1,3},\d{1,3}\)', input)
+    muls = re.findall('mul\(\d{1,3},\d{1,3}\)', text)
     
     # Extract integers.
     nums = [tuple(map(int, re.findall('\d+', mul))) for mul in muls]
